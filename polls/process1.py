b033dc445e8776fe7808de1145dab672cd0fb5a3
David C McGeary <43143579+dmcgeary76@users.noreply.github.com>
2021-06-11 13:53:45 -0500

    added s3 links for static elements

--- polls/process1.py
+++ polls/process1.py
@@ -27,33 +27,10 @@ temp_list = [
 grades_list  = [['PK','K','1','2','3','4','5'],['6','7','8'],['9','10','11','12']]
 
 
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-
 def update_dept(headers):
     dept = []
     dept.append(headers)
-    dept.append(['department','UPDATE','dept_BrightspaceCourses_d2l','Brightspace Courses','','','','','','','',''])
+    dept.append(['department','UPDATE','MTW_Main_2020','Move This World','','','','','','','',''])
     df = pd.DataFrame(dept)
     df.to_csv('1-Department.csv', header=False, index=False, sep=',')
 
