b987d0153969f42a2b22f2e5b98c378cb6ede1d9
David C McGeary <43143579+dmcgeary76@users.noreply.github.com>
2021-06-11 12:50:25 -0500

    added s3 links for static elements

--- polls/process1.py
+++ polls/process1.py
@@ -43,7 +43,7 @@ def upload_file(file_name, bucket, object_name=None):
     # Upload the file
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.upload_file(file_name, bucket, object_name, ExtraArgs={'ACL':'public_read'})
+        response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
         logging.error(e)
         return False
